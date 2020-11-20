ffa05ea24475ce722f4352b10f41531aef4489c1
royal-dargon <2312749625@qq.com>
2020-11-21 01:01:51 +0800

    update

---
+++ test/8/8.2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var drink map[string]string
+	drink = make(map[string]string,5)
+	drink["cola"] = "可乐"
+	drink["coffe"]= "咖啡"
+	drink["tea"]  = "茶"
+	drink["wine"] = "红酒"
+	for key,value := range drink {
+		fmt.Printf("key : %v,value : %v\n",key,value)
+	}
+	keys := make([]string,len(drink))
+	i := 0
+	for k,_ := range drink {
+		keys[i] = k
+		i ++
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("key : %v , value : %v\n",k,drink[k])
+	}
+
+}
