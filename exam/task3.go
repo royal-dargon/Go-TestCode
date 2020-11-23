8edcf7ab613d9eba29e3628371912a8448f10dd1
royal-dargon <2312749625@qq.com>
2020-11-24 01:11:23 +0800

    second

--- exam/task3.go
+++ exam/task3.go
@@ -2,30 +2,33 @@ package main
 
 import "fmt"
 
+//定义结构体
 type cube struct {
 	lenth int
 	wide  int
 	heigh int
 }
 
-func volume (x,y,z int) int {
-	return x * y *z
-}
-
-func area (x,y,z int) int {
-	s1 := x * y
-	s2 := x * z
-	s3 := z * y
+//定义一个求表面积的方法
+func (tn *cube) addarea() int {
+	s1 := tn.lenth * tn.wide
+	s2 := tn.lenth * tn.heigh
+	s3 := tn.wide   * tn.heigh
 	s4 := 2 * s1 + 2 * s2 + 2 * s3
 	return s4
 }
 
-func main() {
-	var cube1 cube
-	fmt.Scanf("%d",&cube1.lenth)
-	fmt.Scanf("%d",&cube1.wide)
-	fmt.Scanf("%d",&cube1.heigh)
-	volume1 := volume(cube1.lenth,cube1.wide,cube1.heigh)
-	area1   := area(cube1.lenth,cube1.wide,cube1.heigh)
-	fmt.Printf("volume : %d ; area : %d\n",volume1,area1)
+//定义一个求体积的方法
+func (ts *cube) addvolume() int {
+	v1 := ts.lenth * ts.wide * ts.heigh
+	return v1
 }
+
+func main() {
+	test1 := new(cube)
+	fmt.Printf("请分别输入长宽高\n")
+	fmt.Scanf("%d %d %d",&test1.lenth,&test1.wide,&test1.heigh)
+	fmt.Println(test1.addarea())
+	fmt.Println(test1.addvolume())
+}
+
