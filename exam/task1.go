d8b28cc92e258061e56a36e16d82863918745395
royal-dargon <2312749625@qq.com>
2020-11-24 01:37:23 +0800

    second

--- exam/task1.go
+++ exam/task1.go
@@ -3,29 +3,22 @@ package main
 import "fmt"
 
 func Replace (s string, target, replace byte) string {
-	var s3 []string
-	var s2 string
-	for n,m := range s {
-		s1 := []byte(s)
-		if byte(m) == target {
-			s1[n] = replace
+	str := []byte(s)
+	for i := 0; i < len(s) ; i ++ {
+		if str[i] == target {
+			str[i] = replace
 		}
-		s3 = append (s3,string(s1[n]))
 	}
-	s2 = string(s3)
-	return s2
+	sreturn := string(str)
+	return sreturn
 }
 
 func main() {
 	var s string
-	var a byte
-	var b byte
+	var a byte = 's'
+	var b byte = 'k'
 	fmt.Printf("请输入一个字符串\n")
 	fmt.Scanf("%s",&s)
-	fmt.Printf("请输入要替换的字符")
-	fmt.Scanf("%c",&a)
-	fmt.Printf("请输入替换后的字符")
-	fmt.Scanf("%c",&b)
-	s3 := Replace(s,a,b)
-	fmt.Printf("%v\n",s3)
+	s2 := Replace(s,a,b)
+	fmt.Println(s2)
 }
